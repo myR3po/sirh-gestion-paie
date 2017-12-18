a37abf8945b1835d623ccdc30dfc7399ba6377dd
myR3po <>
2017-12-18 16:31:43 +0100

    TP 02 Part 2 - Implémentation CalculerRemunerationServiceSimple

--- src/main/java/dev/paie/service/CalculerRemunerationServiceSimple.java
+++ src/main/java/dev/paie/service/CalculerRemunerationServiceSimple.java
@@ -1,9 +1,14 @@
 package dev.paie.service;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import dev.paie.entite.BulletinSalaire;
+import dev.paie.entite.Cotisation;
 import dev.paie.entite.ResultatCalculRemuneration;
+import dev.paie.util.PaieUtils;
 
 @Service
 public class CalculerRemunerationServiceSimple implements CalculerRemunerationService
@@ -11,7 +16,50 @@ public class CalculerRemunerationServiceSimple implements CalculerRemunerationSe
 
 	@Override
 	public ResultatCalculRemuneration calculer(BulletinSalaire bulletin) {
-		return null;
+		PaieUtils paieUtils = new PaieUtils();
+		ResultatCalculRemuneration rcr = new ResultatCalculRemuneration();
+		
+		
+//		SALAIRE_BASE = GRADE.NB_HEURES_BASE * GRADE.TAUX_BASE
+		rcr.setSalaireDeBase(paieUtils.formaterBigDecimal(bulletin.getRemunerationEmploye().getGrade().getNbHeuresBase().multiply(bulletin.getRemunerationEmploye().getGrade().getTauxBase())));
+		BigDecimal salaireBase = new BigDecimal(rcr.getSalaireDeBase());
+
+//		SALAIRE_BRUT = SALAIRE_BASE + PRIME_EXCEPTIONNELLE
+		rcr.setSalaireBrut(paieUtils.formaterBigDecimal(salaireBase.add(bulletin.getPrimeExceptionnelle())));
+		BigDecimal salaireBrut = new BigDecimal(rcr.getSalaireBrut());
+
+//		TOTAL_RETENUE_SALARIALE = SOMME(COTISATION_NON_IMPOSABLE.TAUX_SALARIAL*SALAIRE_BRUT)
+		
+		List<Cotisation> cot = 	bulletin.getRemunerationEmploye().getProfilRemuneration().getCotisationsNonImposables();
+		
+		rcr.setTotalRetenueSalarial(paieUtils.formaterBigDecimal(cot.stream()
+				.filter(c -> c.getTauxSalarial() !=null )
+				.map(c -> c.getTauxSalarial().multiply(salaireBrut))
+				.reduce(BigDecimal::add).get()
+		));
+
+//		TOTAL_COTISATIONS_PATRONALES = SOMME(COTISATION_NON_IMPOSABLE.TAUX_PATRONAL*SALAIRE_BRUT)
+		rcr.setTotalCotisationsPatronales(paieUtils.formaterBigDecimal(cot.stream()
+				.filter(c -> c.getTauxPatronal() !=null)
+				.map(c -> c.getTauxPatronal().multiply(salaireBrut))
+				.reduce(BigDecimal::add).get()
+		));
+		
+//		NET_IMPOSABLE = SALAIRE_BRUT - TOTAL_RETENUE_SALARIALE
+		rcr.setNetImposable(paieUtils.formaterBigDecimal(salaireBrut.subtract(new BigDecimal(rcr.getTotalRetenueSalarial()))));
+		
+//		NET_A_PAYER = NET_IMPOSABLE - SOMME(COTISATION_IMPOSABLE.TAUX_SALARIAL*SALAIRE_BRUT)
+		cot = 	bulletin.getRemunerationEmploye().getProfilRemuneration().getCotisationsImposables();
+		rcr.setNetAPayer(
+				paieUtils.formaterBigDecimal(new BigDecimal(rcr.getNetImposable()).subtract(
+					cot.stream()
+					.filter(c -> c.getTauxSalarial() !=null)
+					.map(c -> c.getTauxSalarial().multiply(salaireBrut))
+					.reduce(BigDecimal::add).get()			
+				)
+			));
+		
+		return rcr;
 	}
 
 }
--- src/test/java/dev/paie/service/CalculerRemunerationServiceSimpleTest.java
+++ src/test/java/dev/paie/service/CalculerRemunerationServiceSimpleTest.java
@@ -8,22 +8,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import dev.paie.config.JeuxDeDonneesConfig;
 import dev.paie.config.ServicesConfig;
+import dev.paie.entite.BulletinSalaire;
 import dev.paie.entite.ResultatCalculRemuneration;
 
 //Sélection des classes de configuration Spring à utiliser lors du test
-@ContextConfiguration(classes = { ServicesConfig.class })
+@ContextConfiguration(classes = { ServicesConfig.class, JeuxDeDonneesConfig.class })
 // Configuration JUnit pour que Spring prenne la main sur le cycle de vie du
 // test
 @RunWith(SpringRunner.class)
 public class CalculerRemunerationServiceSimpleTest {
 	@Autowired
 	private CalculerRemunerationService remunerationService;
+	
+	@Autowired
+	private BulletinSalaire bulletinSalaire;
 
 	@Test
 	public void test_calculer() {
-		// TODO remplacer null par un objet bulletin
-		ResultatCalculRemuneration resultat = remunerationService.calculer(null);
+		ResultatCalculRemuneration resultat = remunerationService.calculer(bulletinSalaire);
 		assertThat(resultat.getSalaireBrut()).isEqualTo("2683.30");
 		assertThat(resultat.getTotalRetenueSalarial()).isEqualTo("517.08");
 		assertThat(resultat.getTotalCotisationsPatronales()).isEqualTo("1096.13");
